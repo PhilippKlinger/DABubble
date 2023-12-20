05c6990038e29a24f0907959bc5015bf034cacbc
ErsanBihorac <ersan.bihorac.2003@gmail.com>
2023-12-20 21:01:04 +0100

    old project gone

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"dabubble-5ec88","appId":"1:540601189295:web:7f3ece8c1beefd7ba9655a","storageBucket":"dabubble-5ec88.appspot.com","apiKey":"AIzaSyBwQtoQFZ1UWiUgbOSwrfARsckBgdqZjXA","authDomain":"dabubble-5ec88.firebaseapp.com","messagingSenderId":"540601189295"})),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
