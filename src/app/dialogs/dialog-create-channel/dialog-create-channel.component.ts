6eec5044dd6b0857007272d2556018d794ab6eba
PhilippKlinger <klinger.breuberg@web.de>
2023-12-23 00:47:10 +0100

    added html/scss to dialog-create-channel

--- src/app/dialogs/dialog-create-channel/dialog-create-channel.component.ts
+++ src/app/dialogs/dialog-create-channel/dialog-create-channel.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-dialog-create-channel',
   templateUrl: './dialog-create-channel.component.html',
-  styleUrls: ['./dialog-create-channel.component.scss']
+  styleUrls: ['./dialog-create-channel.component.scss',
+    '../dialog-show-profile/dialog-show-profile.component.scss',
+    '../dialog-edit-profile/dialog-edit-profile.component.scss']
 })
 export class DialogCreateChannelComponent {
 
