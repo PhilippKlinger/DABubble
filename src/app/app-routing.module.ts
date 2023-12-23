ea3f0c43ab4cf6769aef197878f5ae72e030173b
Martin Unger <martin-unger2@live.at>
2023-12-23 19:22:21 +0100

    login component styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DialogDummyComponent } from './dialogs/dialog-dummy/dialog-dummy.component';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: '', component: AppComponent },      //nur platzhalter
+  { path: 'login', component: LoginComponent },
   { path: 'dialogs', component: DialogDummyComponent },
 ];
 
