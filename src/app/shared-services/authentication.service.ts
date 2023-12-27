70ccf7b89ddead512d9702029062cfa904e92313
Martin Unger <martin-unger2@live.at>
2023-12-27 19:41:25 +0100

    h1 font-family fix start autentication

---
+++ src/app/shared-services/authentication.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import firebase from 'firebase/compat/app';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  constructor(private afAuth: AngularFireAuth) {}
+
+  async login(email: string, password: string) {
+    return this.afAuth.signInWithEmailAndPassword(email, password);
+  }
+
+  async register(email: string, password: string) {
+    return this.afAuth.createUserWithEmailAndPassword(email, password);
+  }
+
+  async loginWithGoogle() {
+    return this.afAuth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+
+  async resetPassword(email: string) {
+    return this.afAuth.sendPasswordResetEmail(email);
+  }
+}
