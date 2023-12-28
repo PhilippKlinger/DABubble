4ec1535f50f5da67f88e22bf5f5e994e37b74e35
ErsanBihorac <ersan.bihorac.2003@gmail.com>
2023-12-28 21:02:26 +0100

    new dialogs added

--- src/app/main-content/main-content-side-bar/main-content-side-bar.component.ts
+++ src/app/main-content/main-content-side-bar/main-content-side-bar.component.ts
@@ -1,4 +1,11 @@
+import { ComponentType } from '@angular/cdk/portal';
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogCreateChannelComponent } from 'src/app/dialogs/dialog-create-channel/dialog-create-channel.component';
+import { DialogEditChannelComponent } from 'src/app/dialogs/dialog-edit-channel/dialog-edit-channel.component';
+import { DialogEditProfileComponent } from 'src/app/dialogs/dialog-edit-profile/dialog-edit-profile.component';
+import { DialogMenuProfileComponent } from 'src/app/dialogs/dialog-menu-profile/dialog-menu-profile.component';
+import { DialogShowProfileComponent } from 'src/app/dialogs/dialog-show-profile/dialog-show-profile.component';
 
 @Component({
   selector: 'app-main-content-side-bar',
@@ -6,5 +13,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./main-content-side-bar.component.scss']
 })
 export class MainContentSideBarComponent {
+  /**
+ * Mapping of component keys to their corresponding Angular component types.
+ *
+ * @type {Record<string, ComponentType<unknown>>}
+ */
+  dialogComponents: Record<string, ComponentType<unknown>> = {
+    'showProfile': DialogShowProfileComponent,
+    'menuProfile': DialogMenuProfileComponent,
+    'editProfile': DialogEditProfileComponent,
+    'createChannel': DialogCreateChannelComponent,
+    'editChannel': DialogEditChannelComponent,
+  };
+  /**
+ * Creates an instance of OpenDialogService.
+ *
+ * @constructor
+ * @param {MatDialog} dialog - Angular Material dialog service.
+ */
+  constructor(private dialog: MatDialog) { }
 
+  /**
+   * Opens a dialog with the specified component key.
+   *
+   * @param {string} componentKey - Key corresponding to the desired dialog component.
+   * @returns {void}
+   */
+  openDialog(componentKey: string): void {
+    const selectedComponent = this.dialogComponents[componentKey];
+    if (selectedComponent) {
+      this.dialog.open(selectedComponent);
+    } else {
+      console.error(`Component with key ${componentKey} not found.`);
+    }
+  }
 }
