a2a7f5cf0b53e7c52dbf915eebf82b2422ff8d47
PhilippKlinger <klinger.breuberg@web.de>
2023-12-29 22:34:05 +0100

    updated main-content-side-bar

--- src/app/main-content/main-content-side-bar/main-content-side-bar.component.ts
+++ src/app/main-content/main-content-side-bar/main-content-side-bar.component.ts
@@ -1,6 +1,4 @@
-import { ComponentType } from '@angular/cdk/portal';
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
+import { Component } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Channel } from 'src/app/models/channel.class';
 import { ChannelsService } from 'src/app/shared-services/channels.service';
@@ -12,7 +10,7 @@ import { OpenDialogService } from 'src/app/shared-services/open-dialog.service';
   templateUrl: './main-content-side-bar.component.html',
   styleUrls: ['./main-content-side-bar.component.scss']
 })
-export class MainContentSideBarComponent implements OnInit {
+export class MainContentSideBarComponent {
   channel_icon: string = 'arrow_drop_down';
   channels_opened: boolean = true;
   directmessage_icon: string = 'arrow_drop_down';
@@ -22,10 +20,7 @@ export class MainContentSideBarComponent implements OnInit {
   unsubChannels!: Subscription;
 
   constructor(private dialogService: OpenDialogService, private channelsService: ChannelsService) {
-  }
-
-  ngOnInit(): void {
-   this.unsubChannels = this.channelsService.channels$.subscribe(channels => {
+    this.unsubChannels = this.channelsService.channels$.subscribe(channels => {
       this.channels = channels;
     });
   }
