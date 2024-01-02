11f149232287c3de9aee70d7c532dea49872b0fe
ErsanBihorac <ersan.bihorac.2003@gmail.com>
2024-01-02 03:04:31 +0100

    open dialog added

--- src/app/main-content/main-content-profile-selector/main-content-profile-selector.component.ts
+++ src/app/main-content/main-content-profile-selector/main-content-profile-selector.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OpenDialogService } from 'src/app/shared-services/open-dialog.service';
 
 @Component({
   selector: 'app-main-content-profile-selector',
@@ -8,7 +9,13 @@ import { Component } from '@angular/core';
 export class MainContentProfileSelectorComponent {
   profilemenu_open: boolean = false;
 
+  constructor(private dialogService: OpenDialogService) { }
+
   toggleProfilemenu() {
     this.profilemenu_open = !this.profilemenu_open;
+  }
+
+  openDialog(componentKey: string): void {
+    this.dialogService.openDialog(componentKey);
   }
 }
