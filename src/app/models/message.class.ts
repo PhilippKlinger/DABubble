4810388a43944dc0685a47e1c8539f3a34c9988d
ErsanBihorac <ersan.bihorac.2003@gmail.com>
2024-01-03 00:41:54 +0100

    messages are now sendable

---
+++ src/app/models/message.class.ts
@@ -0,0 +1,34 @@
+export class message {
+    id: string;
+    message: string;
+    creator: string;
+    timestamp: string | number;
+
+    constructor(obj?: any) {
+        this.id = obj ? obj.id : '';
+        this.message = obj ? obj.message : '';
+        this.creator = obj ? obj.creator : '';
+        this.timestamp = obj ? obj.timestamp : '';
+    }
+
+    setTimestampNow(): void {
+        this.timestamp = Date.now();
+    }
+
+    setCreator(): void {
+        this.creator = 'Gast';
+    }
+
+    setMessage(message:string): void {
+        this.message = message;
+    }
+
+    public toJSON() {
+        return {
+            id: this.id,
+            message: this.message,
+            creator: this.creator,
+            timestamp: this.timestamp
+        }
+    }
+}
