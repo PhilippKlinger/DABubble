6804ae55b95a1ac5081f23975f511af310671e88
ErsanBihorac <ersan.bihorac.2003@gmail.com>
2024-01-12 23:57:26 +0100

    the user's name Avatar and online status is now being displayed in the top right corner

--- src/app/main-content/main-content-profile-selector/main-content-profile-selector.component.ts
+++ src/app/main-content/main-content-profile-selector/main-content-profile-selector.component.ts
@@ -3,6 +3,8 @@ import { OpenDialogService } from 'src/app/shared-services/open-dialog.service';
 import { AuthService } from '../../shared-services/authentication.service';
 import { Router } from '@angular/router';
 import { UserService } from 'src/app/shared-services/user.service';
+import { ChannelsService } from 'src/app/shared-services/channels.service';
+import { User } from 'src/app/models/user.class';
 
 @Component({
   selector: 'app-main-content-profile-selector',
@@ -11,8 +13,13 @@ import { UserService } from 'src/app/shared-services/user.service';
 })
 export class MainContentProfileSelectorComponent {
   profilemenu_open: boolean = false;
+  currentUserInfo!: User;
 
-  constructor(private dialogService: OpenDialogService, private authService: AuthService, private router: Router, private userService: UserService) {}
+  constructor(private dialogService: OpenDialogService, private authService: AuthService, private router: Router, private userService: UserService, private channelService: ChannelsService) {
+    this.channelService.currentUserInfo$.subscribe(user => {
+      this.currentUserInfo = user;
+    });
+  }
 
   toggleProfilemenu() {
     this.profilemenu_open = !this.profilemenu_open;
@@ -23,7 +30,7 @@ export class MainContentProfileSelectorComponent {
   }
 
   logout() {
-    const userJson = sessionStorage.getItem('user');  
+    const userJson = sessionStorage.getItem('user');
     if (userJson) {
       const user = JSON.parse(userJson);
       this.userService.setUserOnlineStatus(user.id, false)
@@ -31,7 +38,7 @@ export class MainContentProfileSelectorComponent {
           return this.authService.logout();
         })
         .then(() => {
-          sessionStorage.removeItem('user'); 
+          sessionStorage.removeItem('user');
           this.router.navigate(['/login']);
         })
         .catch(error => {
