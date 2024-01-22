eb1c86bc17b09579c5122b5bfdb20625c6b71d18
ErsanBihorac <ersan.bihorac.2003@gmail.com>
2024-01-22 06:01:26 +0100

    thread-chat closes now if a different channel is being selected

--- src/app/shared-services/data.service.ts
+++ src/app/shared-services/data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { Message } from '../models/message.class';
+import { ChannelsService } from './channels.service';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +9,12 @@ import { Message } from '../models/message.class';
 export class DataService {
   public thread_open$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
   public directmessage_open$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+
+  constructor(private channelService: ChannelsService) {
+    this.channelService.selectedChannel$.subscribe(() => {
+      this.thread_open$.next(false);
+    });
+  }
 
   getBooleanValue(): boolean {
     return this.thread_open$.value;
